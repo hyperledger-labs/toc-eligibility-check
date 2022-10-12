61a2cc205477e666317d39e6914e844350210e4f
Tracy Kuhrt <tracy.a.kuhrt@accenture.com>
2022-10-12 10:15:36 -0700

    Support voter and nominee eligibility check (#3)
    * Support voter and nominee eligibility check
    
    Signed-off-by: Tracy Kuhrt <tracy.a.kuhrt@accenture.com>

--- hashes.js
+++ hashes.js
@@ -2,10 +2,13 @@
 
 class Hashes {
     static check(hash) {
-        const hashes = {
-              "1e71edbf25822df394f992c490bf0034":"",
-        };
-        return hashes.hasOwnProperty(hash);
+        const voter_hashes = [
+              "1e71edbf25822df394f992c490bf0034","a11026215857ed0e732ed097493a7c38","bcebb1e74752ba81c3b3541dcac6548f"
+        ];
+        const nominee_hashes = [
+              "1e71edbf25822df394f992c490bf0034","a11026215857ed0e732ed097493a7c38","aedee66aab0768ef697372bbb2c3bc72"
+        ];
+        return [(voter_hashes.indexOf(hash) != -1), (nominee_hashes.indexOf(hash) != -1)];
     }
 }
 if (typeof module != 'undefined' && module.exports) module.exports = Hashes;
